b21137b0a39e2baa67e18ce1b9df8fc7287e58ea
WuQianRui <18033405861@163.com>
2026-06-04 21:04:37 +0800

    SignalUtils 新增 FFTK-4499 频域能量谱分析算法

--- utils/SignalUtils.java
+++ utils/SignalUtils.java
@@ -20,4 +20,19 @@ public class SignalUtils {
         for (double v : data) sum += v * v;
         return Math.sqrt(sum / data.length);
     }
+
+    public static double[] fftk4499EnergySpectrum(double[] data, double sampleRate) {
+        int n = data.length;
+        double[] spectrum = new double[n / 2];
+        for (int i = 0; i < spectrum.length; i++) {
+            double real = 0, imag = 0;
+            for (int j = 0; j < n; j++) {
+                double angle = 2 * Math.PI * i * j / n;
+                real += data[j] * Math.cos(angle);
+                imag -= data[j] * Math.sin(angle);
+            }
+            spectrum[i] = Math.sqrt(real * real + imag * imag) / n;
+        }
+        return spectrum;
+    }
 }
