28889f1287268b7ade39b59461efa677d24f53b7
WuQianRui <18033405861@163.com>
2026-06-04 21:05:11 +0800

    OrbitalUtils 新增 Kepler 轨道传播算法实现

--- utils/OrbitalUtils.java
+++ utils/OrbitalUtils.java
@@ -10,4 +10,21 @@ public class OrbitalUtils {
     public static double calculateVelocity(double r, double a) {
         return Math.sqrt(MU * (2.0 / r - 1.0 / a));
     }
+
+    public static double[] keplerPropagate(double a, double e, double M0, double t) {
+        double n = Math.sqrt(MU / Math.pow(a, 3));
+        double M = M0 + n * t;
+        double E = solveKepler(M, e);
+        double trueAnomaly = 2 * Math.atan2(Math.sqrt(1 + e) * Math.sin(E / 2),
+                                            Math.sqrt(1 - e) * Math.cos(E / 2));
+        return new double[]{trueAnomaly, E};
+    }
+
+    private static double solveKepler(double M, double e) {
+        double E = M;
+        for (int i = 0; i < 10; i++) {
+            E = E - (E - e * Math.sin(E) - M) / (1 - e * Math.cos(E));
+        }
+        return E;
+    }
 }
