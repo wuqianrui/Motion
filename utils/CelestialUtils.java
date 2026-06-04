870535011484c33c63335014eba2786dc52a4eda
WuQianRui <18033405861@163.com>
2026-06-04 21:04:14 +0800

    添加 CelestialUtils 天文时间计算模块

---
+++ utils/CelestialUtils.java
@@ -0,0 +1,13 @@
+﻿package com.motion.utils;
+
+public class CelestialUtils {
+    private static final double J2000 = 2451545.0;
+
+    public static double daysSinceJ2000(long timestampMs) {
+        return (timestampMs / 86400000.0) + 2440587.5 - J2000;
+    }
+
+    public static double calculateJulianDate(long timestampMs) {
+        return (timestampMs / 86400000.0) + 2440587.5;
+    }
+}
