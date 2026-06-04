0ca11c59d6f89f81d4f2cf911977269d199de91b
WuQianRui <18033405861@163.com>
2026-06-04 21:04:06 +0800

    添加 GeoidUtils 大地坐标系转换工具

---
+++ utils/GeoidUtils.java
@@ -0,0 +1,17 @@
+﻿package com.motion.utils;
+
+public class GeoidUtils {
+    public static double WGS84_A = 6378137.0;
+    public static double WGS84_F = 1.0 / 298.257223563;
+
+    public static double[] ecefToGeodetic(double x, double y, double z) {
+        double[] result = new double[3];
+        double lon = Math.atan2(y, x);
+        double p = Math.sqrt(x * x + y * y);
+        double lat = Math.atan2(z, p * (1.0 - WGS84_F));
+        result[0] = Math.toDegrees(lat);
+        result[1] = Math.toDegrees(lon);
+        result[2] = 0.0;
+        return result;
+    }
+}
