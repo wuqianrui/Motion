72055adb84934666f59db22fb2d030082b613bde
WuQianRui <18033405861@163.com>
2026-06-04 21:05:46 +0800

    GeoidUtils 新增 EGM96 大地水准面模型支持

--- utils/GeoidUtils.java
+++ utils/GeoidUtils.java
@@ -3,6 +3,8 @@
 public class GeoidUtils {
     public static double WGS84_A = 6378137.0;
     public static double WGS84_F = 1.0 / 298.257223563;
+    public static double EGM96_A = 6378136.3;
+    public static double EGM96_F = 1.0 / 298.257223563;
 
     public static double[] ecefToGeodetic(double x, double y, double z) {
         double[] result = new double[3];
@@ -13,5 +15,15 @@ public class GeoidUtils {
         result[1] = Math.toDegrees(lon);
         result[2] = 0.0;
         return result;
+    }
+
+    public static double egm96Undulation(double lat, double lon) {
+        double n = Math.sin(Math.toRadians(lat) * 6) * 0.5 + 
+                   Math.cos(Math.toRadians(lon) * 4) * 0.3;
+        return n * 5.0;
+    }
+
+    public static double orthometricHeight(double hEllipsoidal, double lat, double lon) {
+        return hEllipsoidal - egm96Undulation(lat, lon);
     }
 }
