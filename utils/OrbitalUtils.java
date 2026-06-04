c5fa91d146477c7d1c53a2258ff6bbd01c121dd1
WuQianRui <18033405861@163.com>
2026-06-04 21:03:55 +0800

    添加 OrbitalUtils 轨道计算基础算法

---
+++ utils/OrbitalUtils.java
@@ -0,0 +1,13 @@
+﻿package com.motion.utils;
+
+public class OrbitalUtils {
+    private static final double MU = 3.986004418e14;
+
+    public static double calculateOrbitalPeriod(double semiMajorAxis) {
+        return 2 * Math.PI * Math.sqrt(Math.pow(semiMajorAxis, 3) / MU);
+    }
+
+    public static double calculateVelocity(double r, double a) {
+        return Math.sqrt(MU * (2.0 / r - 1.0 / a));
+    }
+}
