b7b462114d34c2fb2be3ba47f61a70ca89046767
WuQianRui <18033405861@163.com>
2026-06-04 21:06:27 +0800

    添加 GeoUtils 单元测试用例

---
+++ test/com/motion/utils/GeoUtilsTest.java
@@ -0,0 +1,11 @@
+﻿package com.motion.utils;
+
+public class GeoUtilsTest {
+    public static void main(String[] args) {
+        double dist = GeoUtils.calculateDistance(31.2304, 121.4737, 35.6762, 139.6503);
+        System.out.println("Distance: " + dist);
+        
+        boolean valid = GeoUtils.isValidCoordinate(31.2304, 121.4737);
+        System.out.println("Valid: " + valid);
+    }
+}
